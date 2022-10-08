8493c28667dad8320ab36d5943447b8aeb9b5d72
Boris Diakur <borisdiakur@gmail.com>
2022-10-08 22:39:19 +0200

    refactor: remove unused component imports

--- src/index.tsx
+++ src/index.tsx
@@ -11,11 +11,8 @@ setAssetPath(window.location.origin)
 // TODO: load components lazily where needed.
 const modules = await Promise.all([
   import('@emdgroup-liquid/liquid/dist/components/ld-bg-cells'),
-  import('@emdgroup-liquid/liquid/dist/components/ld-breadcrumbs'),
-  import('@emdgroup-liquid/liquid/dist/components/ld-crumb'),
   import('@emdgroup-liquid/liquid/dist/components/ld-badge'),
   import('@emdgroup-liquid/liquid/dist/components/ld-button'),
-  import('@emdgroup-liquid/liquid/dist/components/ld-checkbox'),
   import('@emdgroup-liquid/liquid/dist/components/ld-icon'),
   import('@emdgroup-liquid/liquid/dist/components/ld-input'),
   import('@emdgroup-liquid/liquid/dist/components/ld-input-message'),
@@ -23,11 +20,7 @@ const modules = await Promise.all([
   import('@emdgroup-liquid/liquid/dist/components/ld-loading'),
   import('@emdgroup-liquid/liquid/dist/components/ld-link'),
   import('@emdgroup-liquid/liquid/dist/components/ld-notification'),
-  import('@emdgroup-liquid/liquid/dist/components/ld-option'),
-  import('@emdgroup-liquid/liquid/dist/components/ld-option-internal'),
   import('@emdgroup-liquid/liquid/dist/components/ld-progress'),
-  import('@emdgroup-liquid/liquid/dist/components/ld-select-popper'),
-  import('@emdgroup-liquid/liquid/dist/components/ld-select'),
   import('@emdgroup-liquid/liquid/dist/components/ld-sidenav'),
   import('@emdgroup-liquid/liquid/dist/components/ld-sidenav-header'),
   import('@emdgroup-liquid/liquid/dist/components/ld-sidenav-heading'),
@@ -38,8 +31,6 @@ const modules = await Promise.all([
   import('@emdgroup-liquid/liquid/dist/components/ld-sr-only'),
   import('@emdgroup-liquid/liquid/dist/components/ld-stepper'),
   import('@emdgroup-liquid/liquid/dist/components/ld-step'),
-  import('@emdgroup-liquid/liquid/dist/components/ld-tooltip'),
-  import('@emdgroup-liquid/liquid/dist/components/ld-tooltip-popper'),
   import('@emdgroup-liquid/liquid/dist/components/ld-typo'),
 ])
 modules.forEach((module) => {
