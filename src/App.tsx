d99204bd4462ba85ef5542b0b16d4f836ad77d2e
Boris Diakur <borisdiakur@gmail.com>
2022-10-27 11:14:30 +0200

    feat: terms

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Home from './pages/Home/Home'
 import Login from './pages/Login/Login'
 import Recover from './pages/Recover/Recover'
 import SignUp from './pages/SignUp/SignUp'
+import Terms from './pages/Terms/Terms'
 import ToDo from './pages/ToDo/ToDo'
 import { Routes, Route } from '@solidjs/router'
 import type { Component } from 'solid-js'
@@ -23,6 +24,7 @@ const App: Component = () => {
           <Route path="/login" component={Login} />
           <Route path="/recover" component={Recover} />
           <Route path="/signup" component={SignUp} />
+          <Route path="/terms" component={Terms} />
         </Routes>
       </div>
       <Routes>
---
+++ src/pages/Terms/Terms.tsx
@@ -0,0 +1,139 @@
+import Aside from '../../components/Aside/Aside'
+import Logo from '../../components/Logo/Logo'
+import type { Component } from 'solid-js'
+
+const Terms: Component = () => {
+  return (
+    <div class="flex w-full max-h-screen">
+      <Aside>
+        <Logo tag="div" href="/" class="mb-ld-16" />
+      </Aside>
+
+      <main
+        aria-live="polite"
+        class="flex flex-grow justify-center px-ld-24 py-ld-40 min-h-screen sm:px-ld-40 lg:min-h-fit overflow-auto"
+      >
+        <div class="container flex-grow mx-auto relative max-w-2xl flex flex-col">
+          <Logo
+            tag="div"
+            href="/"
+            class="mb-ld-16 self-start block lg:hidden"
+          />
+
+          <div class="mb-auto pb-24">
+            <ld-typo variant="h1" class="block my-ld-40">
+              Terms and Conditions
+            </ld-typo>
+
+            <ld-typo class="block mb-ld-32">
+              What follows below is a pretty long lorem ipsum. Its purpose is to
+              give you an impression of what a page containing a text desert
+              would feel like.
+            </ld-typo>
+
+            <ld-typo variant="h4" tag="h2" class="mb-4">
+              The standard Lorem Ipsum passage, used since the 1500s
+            </ld-typo>
+            <ld-typo class="mb-8">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </ld-typo>
+
+            <ld-typo variant="h4" tag="h2" class="mb-4">
+              Section 1.10.32 of de Finibus Bonorum et Malorum, written by
+              Cicero in 45 BC
+            </ld-typo>
+            <ld-typo class="mb-8">
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+              quae ab illo inventore veritatis et quasi architecto beatae vitae
+              dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+              aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
+              eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam
+              est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci
+              velit, sed quia non numquam eius modi tempora incidunt ut labore
+              et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima
+              veniam, quis nostrum exercitationem ullam corporis suscipit
+              laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem
+              vel eum iure reprehenderit qui in ea voluptate velit esse quam
+              nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo
+              voluptas nulla pariatur?
+            </ld-typo>
+
+            <ld-typo variant="h4" tag="h2" class="mb-4">
+              1914 translation by H. Rackham
+            </ld-typo>
+            <ld-typo class="mb-8">
+              But I must explain to you how all this mistaken idea of denouncing
+              pleasure and praising pain was born and I will give you a complete
+              account of the system, and expound the actual teachings of the
+              great explorer of the truth, the master-builder of human
+              happiness. No one rejects, dislikes, or avoids pleasure itself,
+              because it is pleasure, but because those who do not know how to
+              pursue pleasure rationally encounter consequences that are
+              extremely painful. Nor again is there anyone who loves or pursues
+              or desires to obtain pain of itself, because it is pain, but
+              because occasionally circumstances occur in which toil and pain
+              can procure him some great pleasure. To take a trivial example,
+              which of us ever undertakes laborious physical exercise, except to
+              obtain some advantage from it? But who has any right to find fault
+              with a man who chooses to enjoy a pleasure that has no annoying
+              consequences, or one who avoids a pain that produces no resultant
+              pleasure?
+            </ld-typo>
+
+            <ld-typo variant="h4" tag="h2" class="mb-4">
+              Section 1.10.33 of de Finibus Bonorum et Malorum, written by
+              Cicero in 45 BC
+            </ld-typo>
+            <ld-typo class="mb-8">
+              At vero eos et accusamus et iusto odio dignissimos ducimus qui
+              blanditiis praesentium voluptatum deleniti atque corrupti quos
+              dolores et quas molestias excepturi sint occaecati cupiditate non
+              provident, similique sunt in culpa qui officia deserunt mollitia
+              animi, id est laborum et dolorum fuga. Et harum quidem rerum
+              facilis est et expedita distinctio. Nam libero tempore, cum soluta
+              nobis est eligendi optio cumque nihil impedit quo minus id quod
+              maxime placeat facere possimus, omnis voluptas assumenda est,
+              omnis dolor repellendus. Temporibus autem quibusdam et aut
+              officiis debitis aut rerum necessitatibus saepe eveniet ut et
+              voluptates repudiandae sint et molestiae non recusandae. Itaque
+              earum rerum hic tenetur a sapiente delectus, ut aut reiciendis
+              voluptatibus maiores alias consequatur aut perferendis doloribus
+              asperiores repellat.
+            </ld-typo>
+
+            <ld-typo variant="h4" tag="h2" class="mb-4">
+              1914 translation by H. Rackham
+            </ld-typo>
+            <ld-typo>
+              On the other hand, we denounce with righteous indignation and
+              dislike men who are so beguiled and demoralized by the charms of
+              pleasure of the moment, so blinded by desire, that they cannot
+              foresee the pain and trouble that are bound to ensue; and equal
+              blame belongs to those who fail in their duty through weakness of
+              will, which is the same as saying through shrinking from toil and
+              pain. These cases are perfectly simple and easy to distinguish. In
+              a free hour, when our power of choice is untrammelled and when
+              nothing prevents our being able to do what we like best, every
+              pleasure is to be welcomed and every pain avoided. But in certain
+              circumstances and owing to the claims of duty or the obligations
+              of business it will frequently occur that pleasures have to be
+              repudiated and annoyances accepted. The wise man therefore always
+              holds in these matters to this principle of selection: he rejects
+              pleasures to secure other greater pleasures, or else he endures
+              pains to avoid worse pains.
+            </ld-typo>
+          </div>
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export default Terms
